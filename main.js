4d07bd3fa159c20a49aef74b32d3904b43da14e7
Ari Shirwani <158212122+block-firm@users.noreply.github.com>
2024-08-04 23:47:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,10 +12,78 @@ if(searchButton){
     })
 }
 
-// menu hidden
+// search hidden
 
 if(searchClose){
     searchClose.addEventListener('click', ()=> {
         searchContent.classList.remove('show-search')
     })
 }
+
+// login
+
+const loginButton = document.getElementById('login-button'),
+      loginClose = document.getElementById('login-close'),
+      loginContent = document.getElementById('login-content')
+ 
+// login show
+
+if(loginButton){
+    loginButton.addEventListener('click', () =>{
+        loginContent.classList.add('show-login')
+    })
+}
+
+// login hidden
+
+if(loginClose){
+    loginClose.addEventListener('click', ()=> {
+        loginContent.classList.remove('show-login')
+    })
+}
+
+// shadow header
+
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // When scroll is > than 50 viewport height. add the shadow
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                        :header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+// swiper
+
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        1220: {
+            spaceBetween: -32,
+        }
+    }
+  });
+
+  let swiperFeatured = new Swiper('.featured__swiper', {
+    loop: true,
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    breakpoints: {
+        1150: {
+            slidesPerView: 4,
+            centeredSlides: false, 
+        }
+    }
+  });
